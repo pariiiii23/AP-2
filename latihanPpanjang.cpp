a2c64ba51a86e11d3b943e38337291c102a3fb4d
pariiiii23 <hmzn2303@gmail.com>
2026-02-28 09:38:14 +0700

    Pertemuan 2  : Operator

---
+++ latihanPpanjang.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    float luas, p, l;
+
+    cout << "Masukkan Panjang : ";
+    cin << p;
+    
+    cout << "Masukan Lebar : ";
+    cin << l;
+
+    luas = p * l;
+    cout << "Luas = " << luas << endl;
+}
